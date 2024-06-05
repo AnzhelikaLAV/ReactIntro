bc7a79b16e21acf1e6f07fdcaac7a74b87e07b08
AnzhelikaLAV <149877049+AnzhelikaLAV@users.noreply.github.com>
2024-06-05 22:00:23 +0200

    changes

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+function Button({ name, type = "button", onClick }) {
+  return (
+    <button
+      className="button"
+      type={type}
+      onClick={onClick}
+    >
+      {name}
+    </button>
+  );
+}
+
+export default Button;
---
+++ src/consultation/Consultation02/Consultation02.jsx
@@ -0,0 +1,9 @@
+import './styles.css'
+
+function Consultatoin02 () {
+(<div>
+<h1>Consultatoin02</h1>
+</div>)
+}
+
+export default Consultatoin02
